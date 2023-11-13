b5255ade955e0dce51a0fe7dfd5b52b5d3ba6e8a
Tommaso Ascani <tommaso.ascani@gmail.com>
2023-11-13 18:02:15 +0100

    General refactor

--- app/src/main/java/oop23_1010/App.java
+++ app/src/main/java/oop23_1010/App.java
@@ -1,12 +1,24 @@
 package oop23_1010;
 
-import oop23_1010.core.GameEngineImpl;
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
 
     public static void main(String[] args) {
-        GameEngineImpl engine = new GameEngineImpl();
-        engine.setup();
-        engine.mainLoop();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(ClassLoader.getSystemResource("scene.fxml"));
+        FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("scene.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("1010");
+        stage.setScene(new Scene(root, 300, 300));
+        stage.show();
     }
 }
