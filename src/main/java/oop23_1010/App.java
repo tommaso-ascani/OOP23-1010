60f4dc3074018297e769234211f92dbf42d956c5
Tommaso Ascani <tommaso.ascani@gmail.com>
2023-11-15 18:22:23 +0100

    File structure refactor

---
+++ src/main/java/oop23_1010/App.java
@@ -0,0 +1,18 @@
+package oop23_1010;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import oop23_1010.controllers.Startup;
+import oop23_1010.utils.JsonUtils;
+
+public class App {    
+    public static void main(String[] args) {
+        // Application.launch(Startup.class, args);
+        try {
+            JsonUtils.saveData("match");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/oop23_1010/utils/JsonUtils.java
@@ -0,0 +1,34 @@
+package oop23_1010.utils;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JsonUtils {
+
+    private static final String DATA_PATH = "src/main/resources/config/";
+
+    public static void loadData(){
+
+    }
+
+    public static void saveData(final String filePath) throws IOException{
+        try (FileWriter file = new FileWriter(DATA_PATH + filePath + ".json")) {
+            JSONObject json1 = new JSONObject();
+            JSONObject json2 = new JSONObject();
+
+            json1.put("prova1", "ciao1");
+            json1.put("prova1", "ciao1");
+
+            json2.put("salto", json2);
+
+            JSONArray array = new JSONArray();
+            array.put(json2);
+
+            // file.write(array.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
