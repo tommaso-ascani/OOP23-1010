148408391dde46603cb46a673d5ce0ec5c39306c
Tommaso Ascani <tommaso.ascani@gmail.com>
2023-11-17 17:49:37 +0100

    DragAndDrop 1st implementation

--- src/main/java/oop23_1010/App.java
+++ src/main/java/oop23_1010/App.java
@@ -1,11 +1,10 @@
 package oop23_1010;
 
-import oop23_1010.utils.JsonUtils;
-import java.io.IOException;
+import oop23_1010.controllers.Startup;
+import javafx.application.Application;
 
 public class App {
-    public static void main(String[] args) throws IOException{
-        // Application.launch(Startup.class, args);
-        System.out.println(JsonUtils.loadData(JsonUtils.MATCH_ON_GOING));
+    public static void main(String[] args) {
+        Application.launch(Startup.class, args);
     }
 }
---
+++ src/main/java/oop23_1010/controllers/Movement.java
@@ -0,0 +1,22 @@
+package oop23_1010.controllers;
+
+import javafx.scene.Node;
+
+public class Movement {
+    
+    private static double startX;
+    private static double startY; 
+
+    public static void makeDraggable(Node node){
+
+        node.setOnMousePressed(e -> {
+            startX = e.getSceneX() - node.getTranslateX();
+            startY = e.getSceneY() - node.getTranslateY();
+        });
+
+        node.setOnMouseDragged(e -> {
+            node.setTranslateX(e.getSceneX() - startX);
+            node.setTranslateY(e.getSceneY() - startY);
+        });
+    }
+}
