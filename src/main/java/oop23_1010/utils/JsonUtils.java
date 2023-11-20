2586da650a3d4dfb74b1fa388a7954f52bbd6a71
Tommaso Ascani <tommaso.ascani@gmail.com>
2023-11-20 10:51:47 +0100

    New jsonUtils methods

--- src/main/java/oop23_1010/utils/JsonUtils.java
+++ src/main/java/oop23_1010/utils/JsonUtils.java
@@ -3,6 +3,7 @@ package oop23_1010.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import javafx.util.Pair;
 import org.json.JSONObject;
 
 public class JsonUtils {
@@ -13,21 +14,65 @@ public class JsonUtils {
     public static final String MATCH_ON_GOING = "matchOnGoing";
 
     public static Object loadData(String data) throws IOException {
+        // Read file
         String file = Files.readString(Paths.get(DATA_PATH + "match.json"));
+        // Create new JSONObject with file data
         JSONObject json = new JSONObject(file);
+        // Return json given data
         return json.get(data);
     }
 
-    public static void saveMatchData(final boolean matchOnGoing, final int matchScore) throws IOException {
+    public static JSONObject loadDatas() throws IOException {
+        // Read file
+        String file = Files.readString(Paths.get(DATA_PATH + "match.json"));
+        // Create new JSONObject with file data
+        JSONObject json = new JSONObject(file);
+        // Return all json datas
+        return json;
+    }
+
+    public static void saveMatchData(JSONObject json) throws IOException {
         // Create directory if doesn't exist
         Files.createDirectories(Paths.get(DATA_PATH));
+        // Write json on file
+        Files.writeString(Paths.get(DATA_PATH + "match.json"), json.toString(1));
+    }
 
-        // Write match data as a json
-        JSONObject matchData = new JSONObject();
-        
-        matchData.put("matchOnGoing", matchOnGoing);
-        matchData.put("matchScore", matchScore);
+    public static void addElement(Pair<String, Object> temp) throws IOException {
+        // Create local variable
+        JSONObject json;
 
-        Files.writeString(Paths.get(DATA_PATH + "match.json"), matchData.toString(1));
+        if(Files.exists(Paths.get(DATA_PATH + "match.json")) && Files.readAllBytes(Paths.get(DATA_PATH + "match.json")).length > 0){
+            // Get json element if exists
+            json = loadDatas();
+        } else {
+            // Create new JSONObject if doesn't exist
+            json = new JSONObject();
+        }
+
+        // Add new element
+        json.put(temp.getKey(), temp.getValue());
+        // Save data
+        saveMatchData(json);
+    }
+
+    public static void removeElement(String data) throws IOException {
+        // Get json element if exists
+        JSONObject json = loadDatas();
+        // Add new element
+        json.remove(data);
+
+        if(json.isEmpty()){
+            // Delete json file if is empty
+            flushJson();
+        } else {
+            // Save data
+            saveMatchData(json);
+        }
+    }
+
+    public static void flushJson() throws IOException{
+        // Delete json file
+        Files.delete(Paths.get(DATA_PATH + "match.json"));
     }
 }
