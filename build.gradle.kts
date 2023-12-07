2552d5b9226da3bbe06a2db0b678b1443f57c0f0
Tommaso Ascani <tommaso.ascani@gmail.com>
2023-12-07 16:10:37 +0100

    Gradle fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     java
     application
+    // Plugin fro jar file
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("org.danilopianini.gradle-java-qa") version "1.6.0"
+    // Plugin for java automated quality assurance
+    id("org.danilopianini.gradle-java-qa") version "1.28.0"
 }
 
 repositories {
@@ -13,24 +15,10 @@ val javaFXModules = listOf("base", "controls", "fxml", "swing", "graphics", "med
 val supportedPlatforms = listOf("linux", "mac", "win")
 
 dependencies {
-    compileOnly("com.github.spotbugs:spotbugs-annotations:4.7.3")
-
     val junitVersion = "5.9.2"
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
-
-    val javaFakerVersion = "1.0.2"
-    implementation("com.github.javafaker:javafaker:$javaFakerVersion")
-
-    val googleGsonVersion = "2.10.1"
-    implementation("com.google.code.gson:gson:$googleGsonVersion")
-
-    val apacheLang3Version = "3.12.0"
-    implementation("org.apache.commons:commons-lang3:$apacheLang3Version")
-
-    val apacheCollections4Version = "4.4"
-    implementation("org.apache.commons:commons-collections4:$apacheCollections4Version")
 
     implementation ("org.json:json:20231013")
 
